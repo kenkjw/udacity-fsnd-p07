f78a624d6634ff9522251471319eb460df2a65dc
Ken <kenkjw@gmail.com>
2016-11-06 18:56:28 -0800

    Add user registration

--- battleships.py
+++ battleships.py
@@ -1,9 +1,15 @@
 import endpoints
+from protorpc import remote
 
+from models import RegisterUserForm
+from models import StringMessage
+from models import User
 from settings import WEB_CLIENT_ID
 
 EMAIL_SCOPE = endpoints.EMAIL_SCOPE
 API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
+
+USER_REQUEST = endpoints.ResourceContainer(RegisterUserForm)
 
 
 @endpoints.api(
@@ -12,7 +18,26 @@ API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
     allowed_client_ids=[WEB_CLIENT_ID, API_EXPLORER_CLIENT_ID],
     scopes=[EMAIL_SCOPE])
 class BattleshipApi(remote.Service):
-    pass
+    """Battleship Game API"""
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=StringMessage,
+                      path='user',
+                      name='register_user',
+                      http_method='POST')
+    def create_user(self, request):
+        """Create a User. Requires a unique username"""
+        user = endpoints.get_current_user()
+        if User.query(User.email == user.email()).get():
+            raise endpoints.ConflictException(
+                    'You have already registered!')
+
+        if User.query(User.name == request.user_name).get():
+            raise endpoints.ConflictException(
+                    'A User with that name already exists!')
+        user = User(name=request.user_name, email=user.email())
+        user.put()
+        return StringMessage(message='User {} created!'.format(
+                request.user_name))
 
 
 api = endpoints.api_server([BattleshipApi])  # register API
